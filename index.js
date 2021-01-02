69926f7c0201ff417cf715f6c3f90b30a334bb28
Joseph <ojppresido@gmail.com>
2021-01-02 14:12:52 +0800

    First Commit.

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const compression = require('compression');
+const helmet = require('helmet');
+const winston = require('winston');
+require('winston-mongodb');
+require('express-async-errors');
+const config = require('config');
+const error =  require('./middleware/error');
+const mongoose = require('mongoose');
+const staffs = require('./route/staffs');
+const authent = require('./route/authent');
+const express = require('express');
+const app = express();
+
+
+
+process.on('uncaughtException', (ex)=>{
+    console.log('We have Problem in the Startup');
+    winston.error(ex.message, ex);
+});
+winston.add(new winston.transports.File ({filename: 'errors'}));
+winston.add(new winston.transports.MongoDB ({db: 'mongodb://localhost/INECSTAFF'}));
+
+
+const db = config.get('db')
+mongoose.connect('mongodb://localhost/INECSTAFF')
+.then(()=>console.log(`Connected To ${db}`))
+.catch(err=>console.error('Could not connect', err));
+
+
+// throw new Error('something failed during startup');
+app.use(helmet());
+app.use(compression());
+app.use('/staff', staffs);
+app.use('/authent', authent);
+app.use(error);
+
+
+
+
+
+
+
+const server =  app.listen(14, ()=>console.log('Connected to Postman'));
+
+module.exports = server;
