f13b681749a4953459bb9495ae7fdf1630bfa270
Joseph <ojppresido@gmail.com>
2021-01-09 13:39:32 +0800

    first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const helmet = require('helmet');
 // require('winston-mongodb');
 // require('express-async-errors');
 const config = require('config');
-// const error =  require('./middleware/error');
+const error =  require('./middleware/error');
 const mongoose = require('mongoose');
 const staffs = require('./route/staffs');
 const authent = require('./route/authent');
@@ -33,7 +33,7 @@ app.use(helmet());
 app.use(compression());
 app.use('/staff', staffs);
 app.use('/authent', authent);
-// app.use(error);
+app.use(error);
 
 
 
